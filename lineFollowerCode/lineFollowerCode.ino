075c0ea81a4539cf4ad75683edfd778b8c7fdac8
Derek Wider <technoD@outlook.com>
2017-09-17 13:42:40 -0400

    got everythin working, needs tuning

--- lineFollowerCode/lineFollowerCode.ino
+++ lineFollowerCode/lineFollowerCode.ino
@@ -6,15 +6,16 @@
  */
 
 bool is_running = false; 
+bool isTimeUp = false;
 
 //Digital Pin Definitions
-const int GAIN_LED_0 = 2;
-const int GAIN_LED_1 = 3;
+const int MODE_LED_0 = 2;
+const int MODE_LED_1 = 3;
 const int PWR_LED = 4;
 const int MTR_L = 5;
 const int MTR_R = 6;
 const int IN_1_2 = 7;
-const int GAIN_PB = 8;
+const int MODE_PB = 8;
 const int START_PB = 9;
 const int IN_3_4 = 10;
 
@@ -23,130 +24,217 @@ const int SENSE_RIGHT = 0;
 const int SENSE_LEFT = 1;
 const int SENSE_CENTER = 2;
 
-int last_dir = CENTER;
-
 //Analog Threshhold Values
 const int RIGHT_THRESH = 200;
 const int LEFT_THRESH = 200;
-const int CENT_THRESH = 200;
+const int CENT_THRESH = 150;
 
 unsigned long last_detect = 0; //time since last detection
 
-int gain = 1;
+int mode = 1;
 
 void setup() {
-  // configure the outputs on the Arduino
-  pinMode(GAIN_LED_0, OUTPUT);  //Gain mode LED (1)
-  pinMode(GAIN_LED_1, OUTPUT);  //Gain mode LED (0)
-  pinMode(PWR_LED,OUTPUT);  //PWR/status LED
-  pinMode(MTR_L,OUTPUT);  //Mtr 2 (L) PWM
-  pinMode(MTR_R,OUTPUT);  //Mtr 1 (R) PWM
-  pinMode(IN_1_2, OUTPUT);	//Direction Switching for MTR_L
-  pinMode(IN_3_4, OUTPUT);	//Direction Switching for MTR_R
-  pinMode(GAIN_PB,INPUT);   //PB1 (Gain)
-  pinMode(START_PB,INPUT);   //PB2 (Start) 
+	Serial.begin(9600);
+	// configure the outputs on the Arduino
+	pinMode(MODE_LED_0, OUTPUT);  //mode LED (1)
+	pinMode(MODE_LED_1, OUTPUT);  //mode LED (0)
+	pinMode(PWR_LED,OUTPUT);  //PWR/status LED
+	pinMode(MTR_L,OUTPUT);  //Mtr 2 (L) PWM
+	pinMode(MTR_R,OUTPUT);  //Mtr 1 (R) PWM
+	pinMode(IN_1_2, OUTPUT);	//Direction Switching for MTR_L
+	pinMode(IN_3_4, OUTPUT);	//Direction Switching for MTR_R
+	pinMode(MODE_PB,INPUT);   //PB1 (mode)
+	pinMode(START_PB,INPUT);   //PB2 (Start) 
+	
+	//Display default Mode (1):
+	digitalWrite(MODE_LED_0, HIGH);
+	digitalWrite(MODE_LED_1, LOW);
 
 }
 
 void loop() {
-  //if the start button is pressed
-  if(!digitalRead(Start_PB)){ 
-    //wait 1ms for debounce clear, then check again
-    delay(1);
-    if(!digitalRead(Start_PB)){
-      if(is_running){
-        //enable the running LED
-        is_running = true;
-        digitalWrite(PWR_LED, HIGH);
-      }
-      else{            //if running, stop and turn off LED
-        is_running = false;
-        digitalWrite(PWR_LED, LOW);
-        stop_motors();
-      }
-  }
-  if(!digitalRead(Gain_PB)){ //if gain pressed, cycle gain
-    //wait 1ms for debounce clear
-    delay(1);
-    if(!digitalRead(Gain_PB)){
-      if(gain == 3){
-        gain = 1;
-      }
-      else gain++;
-      if(gain == 1){
-        digitalWrite(Gain_LED_1, HIGH);
-        digitalWrite(Gain_LED_2, LOW);
-      }
-      else if(gain == 2){
-        digitalWrite(Gain_LED_1, LOW);
-        digitalWrite(Gain_LED_2, HIGH);
-      }
-      else{
-        digitalWrite(Gain_LED_1, HIGH);
-        digitalWrite(Gain_LED_2, HIGH);
-      }
-    }
-  }
-  if(is_running) 
-  {
-    if(isCentTrig()) //if the center sensor detects the line, drive straight
-    {
-      digitalWrite(MTR_L_In,HIGH);
-      digitalWrite(MTR_R_In,HIGH);
-      last_detect = millis();
-    }
-    else if(isLeftTrig()) //if the left sensor detects the line, turn left
-    {
-      digitalWrite(MTR_L_In,HIGH);
-      digitalWrite(MTR_R_In,LOW);
-      last_detect = millis();
-    }
-    else if(isRightTrig()) //if the right sensor detects the line, turn right
-    {
-      digitalWrite(MTR_L_In,LOW);
-      digitalWrite(MTR_R_In,HIGH);
-    }
-    else //if no sensors detect line
-    {
-      if(millis()-last_detect > 2000) //if more than 2 seconds have passed since last detection, stop
-      {
-        stop_motors();
-        while(digitalRead(Start_PB))
-        {
-          digitalWrite(PWR_LED,1);
+	//if the start button is pressed
+	if(!digitalRead(START_PB)){ 
+		//wait 1ms for debounce clear, then check amode
+		delay(1);
+		if(!digitalRead(START_PB)){
+			if(!is_running){
+				is_running = true;
+				digitalWrite(PWR_LED, HIGH);
+			}
+			//if running, stop and turn off LED
+			else{            
+			is_running = false;
+			digitalWrite(PWR_LED, LOW);
+			stop_motors();
+			}
+		}
+		delay(500);
+	}
+	if(!digitalRead(MODE_PB) && !is_running){ //if mode pressed, cycle mode
+		//wait 1ms for debounce clear
+		delay(1);
+		if(!digitalRead(MODE_PB)){
+			if(mode == 3){
+				mode = 1;
+			}
+			else mode++;
+			switch (mode){
+				case 1: 
+					digitalWrite(MODE_LED_0, HIGH);
+					digitalWrite(MODE_LED_1, LOW);
+					break;
+				case 2: 
+					digitalWrite(MODE_LED_0, LOW);
+					digitalWrite(MODE_LED_1, HIGH);
+					break;
+				case 3:
+					digitalWrite(MODE_LED_0, HIGH);
+					digitalWrite(MODE_LED_1, HIGH);
+					break;
+			}
+		}
+		delay(500);
+	}
+	if(is_running){
+		switch (mode){
+			case 1:
+				if(isCentTrig()){
+					goForwardFast();
+				}
+				else if(isLeftTrig()){
+					//motor status: Both Forward
+					digitalWrite(IN_1_2, HIGH);
+					digitalWrite(IN_3_4, HIGH);
+					//motor speed: Right higher than left
+					analogWrite(MTR_L, 70);
+					analogWrite(MTR_R, 90);
+				}
+				else if(isRightTrig()){
+					//motor status: Both Forward
+					digitalWrite(IN_1_2, HIGH);
+					digitalWrite(IN_3_4, HIGH);
+					//motor speed: Left higher than Right
+					analogWrite(MTR_L, 90);
+					analogWrite(MTR_R, 70);
+				}
+				//If no sensor sees the line
+				else{
+					noLineProcess();
+				}
+				break;
+			case 2:
+				if(isCentTrig()){
+					goForwardFast();
+				}
+				else if(isLeftTrig()){
+					//motor status: Right Forward, Left Backward (off)
+					digitalWrite(IN_1_2, LOW);
+					digitalWrite(IN_3_4, HIGH);
+					//motor speed: Left off, Right forward
+					analogWrite(MTR_L, 0);
+					analogWrite(MTR_R, 130);
+				}
+				else if(isRightTrig()){
+					//motor status: Left Forward, Right Backward (off)
+					digitalWrite(IN_1_2, HIGH);
+					digitalWrite(IN_3_4, LOW);
+					//motor speed: Left higher than Right
+					analogWrite(MTR_L, 130);
+					analogWrite(MTR_R, 0);
+				}
+				//If no sensor sees the line
+				else{
+					noLineProcess();
+				}
+				break;
+			case 3:
+				if(isCentTrig()){
+					goForwardFast();
+				}
+				else if(isLeftTrig()){
+					//motor status: Both Reverse
+					digitalWrite(IN_1_2, LOW);
+					digitalWrite(IN_3_4, HIGH);
+					//motor speed: Left Reverse, Right forward
+					analogWrite(MTR_L, 150);
+					analogWrite(MTR_R, 120);
+				}
+				else if(isRightTrig()){
+					//motor status: Both Reverse
+					digitalWrite(IN_1_2, HIGH);
+					digitalWrite(IN_3_4, LOW);
+					//motor speed: Right Reverse, Left forward
+					analogWrite(MTR_L, 120);
+					analogWrite(MTR_R, 150);
+				}
+				//If no sensor sees the line
+				else{
+					noLineProcess();
+				}
+				break;
+		}
+	}
+}
+void goForwardFast(){
+	//set both motors to forward
+	digitalWrite(IN_1_2, HIGH);
+	digitalWrite(IN_3_4, HIGH);
+	//set motors to same speed
+	analogWrite(MTR_L, 130);
+	analogWrite(MTR_R, 130);
+}
+//call this is no of the line sensors see the line in any mode
+void noLineProcess(){
+	last_detect = millis();
+	digitalWrite(IN_1_2, HIGH);
+	digitalWrite(IN_3_4, HIGH);
+	while(!isRightTrig() && !isLeftTrig() && !isTimeUp){	
+		Serial.println(millis() - last_detect);
+		if((millis() - last_detect) > 2000){
+			isTimeUp = true;
+		}
+		analogWrite(MTR_L, 135);
+		analogWrite(MTR_R, 135);
+	}
+	isTimeUp = false;
+	if(millis() - last_detect >= 2000){
+		is_running = false;
+		last_detect = 0;
+		stop_motors();
+		while(digitalRead(START_PB)){
+          digitalWrite(PWR_LED, HIGH);
           delay(250);
-          digitalWrite(PWR_LED,0);
+          digitalWrite(PWR_LED, LOW);
           delay(250);
         }
-      }
-      last_detect = 0;
-    }
-    //scale PWM as necessary - based on gain variable
-  }
+	}
 }
-
 //funtion to turn off the motors
-void stop_motors() 
-{
+void stop_motors(){
    digitalWrite(MTR_L,LOW);
    digitalWrite(MTR_R,LOW);
 }
 //return true if the right sensor sees the line
 bool isRightTrig(){
-	if(SENSE_RIGHT > RIGHT_THRESH)
+	Serial.println("in right trig");
+	if(analogRead(SENSE_RIGHT) > RIGHT_THRESH){
 		return true;
+	}
 	else return false;
 }
 //return true if the left sensor sees the line
 bool isLeftTrig(){
-	if(SENSE_LEFT > LEFT_THRESH)
+	if(analogRead(SENSE_LEFT) > LEFT_THRESH){
 		return true;
+	}
 	else return false;
 }
 //return true if the center sensor sees the line
 bool isCentTrig(){
-	if(SENSE_CENTER > CENT_THRESH)
+	if(analogRead(SENSE_CENTER) > CENT_THRESH){
 		return true;
+	}
 	else return false;
 }
 
